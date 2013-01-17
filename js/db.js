5b45711344b6152724e81b71dcc6085c3779bb27
Rod Nolan <rodnolan@gmail.com>
2013-01-17 17:13:02 -0500

    displaying data in two separate lists with item click handlers stubbed in also updated bbui to latest

--- js/db.js
+++ js/db.js
@@ -1,6 +1,8 @@
 app.db = function () {
 
 	'use strict';
+//		<div id="sharetext" data-bb-type="item" data-bb-img="./img/Large/White/icon_242.png" data-bb-title="Share Text"></div>
+//		<div id="shareimg" data-bb-type="item" data-bb-img="./img/Large/White/icon_209.png" data-bb-title="Share Image" data-bb-accent-text="file://"></div>
 
 	var pub = {};
 
@@ -12,20 +14,20 @@ app.db = function () {
 	pub.setTestData = function () {
 		console.log('pub.populateTestData');
 
-		var cart, shelf, testData = '', itemsLoaded = pub.getArray('shelf').length;
-
-		if (!itemsLoaded) {
-
-			testData = '['
-				+ '{"name":"Item 1", "price":"0.99/ea", "quantity":"4"},'
-				+ '{"name":"Item 2", "price":"4.99/lb", "quantity":"4"},'
-				+ '{"name":"Item 3", "price":"2.99/kg", "quantity":"2"}'
+		var shelfData = '['
+				+ '{"idx":"1", "name":"Item 1", "price":"0.99/ea", "quantity":"12"},'
+				+ '{"idx":"4", "name":"Item 4", "price":"4.99/lb", "quantity":"4"},'
+				+ '{"idx":"5", "name":"Item 5", "price":"2.99/kg", "quantity":"2"}'
+				+ ']',
+			cartData = '['
+				+ '{"idx":"2", "name":"Item 2", "price":"2.59/kg", "quantity":"1"},'
+				+ '{"idx":"3", "name":"Item 3", "price":"4.99/lb", "quantity":"4"},'
+				+ '{"idx":"6", "name":"Item 6", "price":"1.19/ea", "quantity":"2"}'
 				+ ']';
 				
-			console.log('storing testData: ' + testData);
-			localStorage.setItem('shelf', testData);
-			localStorage.setItem('cart', '');
-		} 
+		console.log('storing sample data');
+		localStorage.setItem('shelf', shelfData);
+		localStorage.setItem('cart', cartData);
 	};
 
 	pub.getArray = function (key) {
@@ -40,13 +42,13 @@ app.db = function () {
 
 	pub.moveItemToCart = function (idx) {
 		console.log('Adding item to cart: ' + idx);
-		var cart = pub.getArray('cart'), 
-			shelf = pub.getArray('shelf'), 
+		var cart = pub.getArray(app.LSK_CART), 
+			shelf = pub.getArray(app.LSK_SHELF), 
 			itemToMove = shelf.splice(idx, 1)[0];
 		if (itemToMove) {
 			cart.push(itemToMove);
-			pub.persist('cart', cart);
-			pub.persist('shelf', shelf);
+			pub.persist(app.LSK_CART, cart);
+			pub.persist(app.LSK_SHELF, shelf);
 		}
 	};
 
