3e6a418f968195e64fd69e3111f30f5f5fadcc6e
Rod Nolan <rodnolan@gmail.com>
2013-01-26 10:30:35 -0500

    splits the constants out into a file

---
+++ js/constants.js
@@ -0,0 +1,8 @@
+var app = {};
+app.INVENTORY = 'listInventory';
+app.LIST = 'listShelf';
+app.CART = 'listCart';
+app.KEY_INVENTORY = 'inventory';
+app.KEY_LIST = 'list';
+app.KEY_CART = 'cart';
+app.DATA_CHANGED_EVENT = 'dataChanged';
