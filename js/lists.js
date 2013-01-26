110ab3dc05f6318f39df66d7f64b6c47eec1939f
Rod Nolan <rodnolan@gmail.com>
2013-01-26 10:24:29 -0500

    fixes an issue with screen flickering when changing from list to list

--- js/lists.js
+++ js/lists.js
@@ -24,54 +24,54 @@ app.lists = function () {
 			newItem,
 			i;
 
-		$(itemsList).fadeOut('fast');
-		itemsList.clear();
+		$(itemsList).fadeOut('fast', function(){
+			itemsList.clear();
+			switch (whichList) {
+				case app.INVENTORY :
+					console.log('inventory items');
+					listItems = app.db.getArray(app.KEY_INVENTORY);
+					screenTitle.setCaption('Inventory');
 
-		switch (whichList) {
-			case app.INVENTORY :
-				console.log('inventory items');
-				listItems = app.db.getArray(app.KEY_INVENTORY);
-				screenTitle.setCaption('Inventory');
+				break;
+				case app.CART :
+					console.log('cart items');
+					listItems = app.db.getArray(app.KEY_CART);
+					screenTitle.setCaption('My Cart');
 
-			break;
-			case app.CART :
-				console.log('cart items');
-				listItems = app.db.getArray(app.KEY_CART);
-				screenTitle.setCaption('My Cart');
+				break;
+				case app.LIST :
+					console.log('list items');
+					listItems = app.db.getArray(app.KEY_LIST);
+					screenTitle.setCaption('Grocery List');
 
-			break;
-			case app.LIST :
-				console.log('list items');
-				listItems = app.db.getArray(app.KEY_LIST);
-				screenTitle.setCaption('Grocery List');
+				break;
+			}
 
-			break;
-		}
+			for (i = 0; i < listItems.length; i++) {
+				listItem = listItems[i];
+				console.log(listItem);
+				newItem = document.createElement('div');
+				newItem.setAttribute('data-item-id', listItem.id);
+				newItem.setAttribute('data-item-idx', i);
+				newItem.setAttribute('data-lst', whichList);
 
-		for (i = 0; i < listItems.length; i++) {
-			listItem = listItems[i];
-			console.log(listItem);
-			newItem = document.createElement('div');
-			newItem.setAttribute('data-item-id', listItem.id);
-			newItem.setAttribute('data-item-idx', i);
-			newItem.setAttribute('data-lst', whichList);
+				newItem.setAttribute('data-bb-type', 'item');
+				newItem.setAttribute('data-bb-style', 'removebuttons');
+				newItem.setAttribute('data-bb-title', listItem.name);
+				newItem.innerHTML = listItem.quantity + " @ " + listItem.price;
+				console.log(newItem);
 
-			newItem.setAttribute('data-bb-type', 'item');
-			newItem.setAttribute('data-bb-style', 'removebuttons');
-			newItem.setAttribute('data-bb-title', listItem.name);
-			newItem.innerHTML = listItem.quantity + " @ " + listItem.price;
-			console.log(newItem);
-
-			newItem.onclick = function(){
-				var selectedItemData = itemsList.selected.dataset;
-				console.log("selected itemId: " + selectedItemData.itemId + " from [" + selectedItemData.itemIdx + "] in list: " + selectedItemData.lst);
-				app.currentList = selectedItemData.lst;
-				window.addEventListener(app.DATA_CHANGED_EVENT, pub.onDataUpdated);
-				app.db.moveItem(selectedItemData.itemIdx, selectedItemData.lst);
+				newItem.onclick = function(){
+					var selectedItemData = itemsList.selected.dataset;
+					console.log("selected itemId: " + selectedItemData.itemId + " from [" + selectedItemData.itemIdx + "] in list: " + selectedItemData.lst);
+					app.currentList = selectedItemData.lst;
+					window.addEventListener(app.DATA_CHANGED_EVENT, pub.onDataUpdated);
+					app.db.moveItem(selectedItemData.itemIdx, selectedItemData.lst);
+				};
+				itemsList.appendItem(newItem);
 			};
-			itemsList.appendItem(newItem);
-		};
-		$(itemsList).fadeIn('fast');
+			$(itemsList).fadeIn('fast');
+		});
 	};
 
 	pub.onDataUpdated = function (event) {
