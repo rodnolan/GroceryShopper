491dd0601585664928c7838847d5a95b23b1a1dd
Rod Nolan <rodnolan@gmail.com>
2013-01-25 22:11:09 -0500

    app now reads inventory from file

--- js/db.js
+++ js/db.js
@@ -7,6 +7,7 @@ app.db = function () {
 
 	pub.clearTestData = function () {
 		localStorage.clear();
+		localStorage.setItem(app.LSK_INVENTORY, '');
 		localStorage.setItem(app.LSK_SHELF, '');
 		localStorage.setItem(app.LSK_CART, '');
 	};
@@ -14,21 +15,41 @@ app.db = function () {
 	pub.setTestData = function () {
 		console.log('pub.populateTestData');
 
-		var shelfData = '['
-				+ '{"id":"1", "name":"Item 1", "price":"0.99/ea", "quantity":"12"},'
-				+ '{"id":"4", "name":"Item 4", "price":"4.99/lb", "quantity":"4"},'
-				+ '{"id":"5", "name":"Item 5", "price":"2.99/kg", "quantity":"2"}'
-				+ ']',
-			cartData = '['
-				+ '{"id":"2", "name":"Item 2", "price":"2.59/kg", "quantity":"1"},'
-				+ '{"id":"3", "name":"Item 3", "price":"4.99/lb", "quantity":"4"},'
-				+ '{"id":"6", "name":"Item 6", "price":"1.19/ea", "quantity":"2"}'
-				+ ']';
-				
-		console.log('storing sample data');
-		localStorage.setItem(app.LSK_SHELF, shelfData);
-		localStorage.setItem(app.LSK_CART, cartData);
+		var filePath = 'data/inventoryData.json',
+			inventoryData = '',
+			shelfData = '',
+			cartData = '';
+ 		
+		$.ajax({
+			type : 'GET',
+			url : filePath,
+			dataType : 'json',
+			success : function (response, status, xhr) {
+			    console.log('  in ajax success: status: ' + status);
+			    console.log(response);
+			    inventoryData = response;
+			},
+			error : function (jqXHR, textStatus, errorThrown) {
+			    console.log('  in ajax error: status: ' + textStatus);
+			    console.log('  in ajax error: error : ' + errorThrown);
+			},
+			complete : function (jqXHR, textStatus) {
+			    console.log('  in ajax complete: textStatus ' + textStatus);
+			    if (inventoryData === '') {
+					console.log('  ajax call did not succeed, using hard-coded inventory');
+					inventoryData = '['
+								+ '{"id":"0", "name":"Placeholder 1", "price":"1.99/ea", "quantity":"1"},'
+								+ '{"id":"1", "name":"Placeholder2 ", "price":"1.99/ea", "quantity":"1"},'
+								+ ']';
+				}
+				localStorage.setItem(app.LSK_INVENTORY, JSON.stringify(inventoryData));
+				localStorage.setItem(app.LSK_SHELF, shelfData);
+				localStorage.setItem(app.LSK_CART, cartData);
+			}
+		});			
 	};
+
+
 
 	pub.getArray = function (key) {
 		console.log('Getting ' + key + ' items.');
@@ -44,29 +65,53 @@ app.db = function () {
 		var src,  
 			dest, 
 			itemToMove,
+			itemToCopy,
+			inventory = pub.getArray(app.LSK_INVENTORY),
 			cart = pub.getArray(app.LSK_CART),
 			shelf = pub.getArray(app.LSK_SHELF);
 
 		switch(srcList) {
+			case app.LIST_INVENTORY :
+				console.log('Copying item to shelf: ' + idx);
+				src = inventory; 
+				dest = shelf;
+				itemToCopy = src[idx];
+				if (itemToCopy) {
+					dest.push(itemToCopy);
+					pub.persist(app.LSK_SHELF, shelf);
+					pub.dispatchDataEvent(app.DATA_CHANGED_EVENT);
+				}
+			break;
 			case app.LIST_SHELF :
 				console.log('Moving item to cart: ' + idx);
 				src = shelf; 
 				dest = cart;
+
+				itemToMove = src.splice(idx, 1)[0];
+				if (itemToMove) {
+					dest.push(itemToMove);
+					pub.persist(app.LSK_INVENTORY, inventory);
+					pub.persist(app.LSK_CART, cart);
+					pub.persist(app.LSK_SHELF, shelf);
+					pub.dispatchDataEvent(app.DATA_CHANGED_EVENT);
+				}
+
 			break;
 			case app.LIST_CART :
 				console.log('Putting item back on shelf: ' + idx);
 				src = cart; 
 				dest = shelf;
+				itemToMove = src.splice(idx, 1)[0];
+				if (itemToMove) {
+					dest.push(itemToMove);
+					pub.persist(app.LSK_INVENTORY, inventory);
+					pub.persist(app.LSK_CART, cart);
+					pub.persist(app.LSK_SHELF, shelf);
+					pub.dispatchDataEvent(app.DATA_CHANGED_EVENT);
+				}
+
 			break;
 		}
-		itemToMove = src.splice(idx, 1)[0];
-		if (itemToMove) {
-			dest.push(itemToMove);
-			pub.persist(app.LSK_CART, cart);
-			pub.persist(app.LSK_SHELF, shelf);
-			pub.dispatchDataEvent(app.DATA_CHANGED_EVENT);
-		}
-
 	};
 
 	pub.persist = function (slot, items) {
